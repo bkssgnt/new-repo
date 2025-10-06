e3e62c326f9aec173a4bd9a7dcaf7df96cd2f2f7
bienn <seguntobiennkyla@gmail.com>
2025-10-06 10:55:59 +0800

    My first commit.

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('my-app');
+  age: number = 0;
+  pwd: boolean = false;
+  name: string = '';
+  gender: any;
+  defaultHolder: string = 'My name is: ';
+  constructor(){
+
+  }
+
+  ngOnChanges() {
+
+  }
+  ngOnInit(){
+    this.age = 5;
+    this.name = 'Bienn';
+  }
+}
